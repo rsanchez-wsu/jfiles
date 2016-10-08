3ec689f9ec704c7a208f6369943073239470bb70
masters1222 <fenderstrat0409@gmail.com>
2016-10-07 20:56:14 -0400

    Addresses Issue #17.
    Initial testing for parsing XML from server to get file names. This will
    later be the names of the "buttons".

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -24,9 +24,22 @@ package gui;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.StringReader;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 
 /**
  * Gui class file that makes a panel with buttons on it. 
@@ -36,14 +49,39 @@ public class Gui {
 	/**
 	 * Main class of GUI.
 	 * @param args The command line argument
+	 * @throws ParserConfigurationException 
+	 * @throws IOException 
+	 * @throws SAXException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
 		// Creates the frame
 		JFrame frame = new JFrame();
 		// Sets size of frame
 		frame.setSize(450, 350);
 		// Where buttons will be place (rows, columns)
 		frame.setLayout(new GridLayout(2, 5));
+		
+		/*
+		 * Some initial testing for parsing XML for file names and types. Uses fake data for now, assuming this will come later.
+		 * Simply logs file name + extension from XML string to console for now.
+		 */
+		//String containing fake XML for parsing testing (output from server issue #17 from milestone 1)
+	    String testXML = "<?xml version=\"1.0\"?><item><name>Test</name><ext>.txt</ext><type>file</type></item>";
+	    
+	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	    DocumentBuilder builder = factory.newDocumentBuilder();
+	    Document doc = builder.parse(new InputSource(new StringReader(testXML)));
+	    
+	    NodeList nodes = doc.getElementsByTagName("item");
+	    
+	    for(int i = 0; i < nodes.getLength(); i++){
+	    	Node nNode = nodes.item(i);
+	    	
+	    	Element eElement = (Element) nNode;
+	    	
+	    	System.out.println(eElement.getElementsByTagName("name").item(0).getTextContent() + eElement.getElementsByTagName("ext").item(0).getTextContent());
+	    	
+	    }
 
 		for (int i = 0; i < 10; i++) {
 			JButton thebutton = new JButton();
