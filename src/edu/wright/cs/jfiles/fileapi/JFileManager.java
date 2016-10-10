43823c92439f65a624a966774cb8ab18a6d9f855
John Wintersohle II <Dorkatron199@aol.com>
2016-10-09 20:51:42 -0400

    Integration Comments Finished
    This commit creates a variety of comments in JFileManager that help to
    clarify what methods can be worked on without collaboration with other
    teams and which methods require information from which teams.
    
    It also updates some java documentation for the class as a whole
    as well as some methods.
    
    The only change in actual functionality comes in the form of the
    creation of a new, private paste method for use in the move method.
    As elaborated on in the java documentation, this is done to avoid
    conflicts with the clipboard.
    
    This finishes the JFileManager side of issue #37.
    
    ref #37

--- src/edu/wright/cs/jfiles/fileapi/JFileManager.java
+++ src/edu/wright/cs/jfiles/fileapi/JFileManager.java
@@ -31,15 +31,32 @@ package edu.wright.cs.jfiles.fileapi;
  * <p>
  * It is the goal of this class to work as a medium of communication between
  * <b>security</b> (such as authentication, authorization, and accounting),
- * <b>maintenance</b> (such as logging), <b>the GUI</b> (such as searching), and
- * <b>JFiles</b>, by incorporating the methods of all of these other APIs into
- * the File API of JFiles.
+ * <b>maintenance</b> (such as logging), <b>user interfaces</b> (such as GUIs,
+ * CLIs, and searching algorithms), and <b>JFiles</b>, by incorporating the
+ * methods of all of these other APIs into the File API of JFiles.
+ * </p>
+ * 
+ * <p>
+ * In other words, this exist to both keep the duties of front-end and back-end
+ * APIs separate by being a middle-ground for them to communicate. This also
+ * means that the system can be more modular. For example, if a team wants to
+ * create a new GUI, this system should be able to accommodate that. Likewise,
+ * the same could be said for a different security API.
  * </p>
  * 
  * <p>
  * This also exists to separate the methods involved with manipulating JFiles
- * from an external point of view from JFiles themselves. This is done so that a
- * GUI or CLI can better make use of the File API.
+ * from an external point of view from JFiles themselves. This is done for both
+ * logical and functional reasons. It may not make sense for a JFile to copy,
+ * delete, or move itself. Creating the commands in this way also means that
+ * this system can be modular.
+ * </p>
+ * 
+ * <p>
+ * It is also important to note that this method does not hold JFiles. If there
+ * is a need for such an object, a JFolder object will be made. This method is
+ * not intended for instantiation, and, as a result, has no constructors. This
+ * method just provides functions to unify different APIs.
  * </p>
  * 
  * @author <b>Team 5:</b>
@@ -49,31 +66,33 @@ package edu.wright.cs.jfiles.fileapi;
  */
 public class JFileManager {
 
+	// This is suppressed until we build in the functionality for this field.
+	// This will most likely be when we make the paste() method functional.
 	/**
 	 * This is the clipboard used when copying JFiles. It is capable of copying
 	 * and pasting multiple JFiles at a time. It is made private so that it can
 	 * only be accessed via the JFileManager methods for the sake of security.
 	 * 
 	 */
-	// This is suppressed until we build in the functionality for this field.
-	// This will most likely be when we make the paste() method.
 	@SuppressWarnings("unused")
 	private JFile[] clipboard;
 
+	/*
+	 * This method entirely depends on copy and delete. It is pretty much done.
+	 */
 	/**
 	 * <p>
 	 * Cuts the file passed in.
 	 * </p>
 	 * 
 	 * <p>
-	 * This method calls copy then delete on the file being passed in. Adds the
-	 * file to the top of the paste stack then moves the file to the trash
+	 * This method calls copy then delete on the file being passed in. Copies
+	 * the files selected to the clipboard then moves the files to the trash
 	 * directory set up during configuration.
 	 * </p>
 	 * 
 	 * @param files
 	 *            The files being cut.
-	 * 
 	 */
 	public void cut(JFile[] files) {
 		copy(files);
@@ -100,18 +119,48 @@ public class JFileManager {
 		clipboard = files.clone();
 	}
 
+	/*
+	 * Collaborate with team 1 on this method. They are developing a way to
+	 * transfer objects from one machine to another. This is not necessary for a
+	 * single machine, but should be built for network pasting from the start.
+	 */
 	/**
 	 * This method will copy the contents of the clipboard into the directory at
 	 * a specified location name.
 	 * 
 	 * @param dirName
 	 *            The name of the directory being pasted to.
-	 * 
 	 */
 	public void paste(String dirName) {
 
 	}
 
+	/**
+	 * This is a version of paste for use in the move method. This version of
+	 * paste is independent of the clipboard, which is important because we want
+	 * the move method to be independent of the clipboard because the use may
+	 * not expect the move command to mess with the clipboard.
+	 * 
+	 * <p>
+	 * This method should be used when files need to be moved from one location
+	 * to another without affecting the clipboard. It is important to note that
+	 * this method should only duplicate the files to the specified location, it
+	 * should not delete those files. The deletion is to be handled in the
+	 * actual move method.
+	 * </p>
+	 * 
+	 * @param files
+	 *            The files being duplicated into a directory.
+	 * @param dirName
+	 *            The directory the files are being duplicated to.
+	 */
+	private void paste(JFile[] files, String dirName) {
+
+	}
+
+	/*
+	 * Should be do-able without collaboration.
+	 */
 	/**
 	 * Deletes a particular JFile object. If the file(s) is/are not in the trash
 	 * bin, it moves it/them to that location. If they are, then it deletes them
@@ -144,6 +193,11 @@ public class JFileManager {
 	 * bit longer, but may make the functionality closer to what we want.
 	 * 
 	 * I suggest we discuss this at some point.
+	 * 
+	 * *************************************************************************
+	 * 
+	 * Collaborate with team 1 on this method. They are developing a way to
+	 * transfer objects from one machine to another.
 	 */
 
 	/**
@@ -155,9 +209,13 @@ public class JFileManager {
 	 *            The name of the directory where files will be moved to.
 	 */
 	public void move(JFile[] files, String dirName) {
-
+		paste(files, dirName);
+		delete(files);
 	}
 
+	/*
+	 * Should be do-able without collaboration.
+	 */
 	/**
 	 * Renames a file.
 	 * 
@@ -171,6 +229,9 @@ public class JFileManager {
 
 	}
 
+	/*
+	 * Should be do-able without collaboration.
+	 */
 	/**
 	 * Opens the file with the name passed in using the default application
 	 * associated with this kind of file. How this is determined will be
@@ -184,6 +245,9 @@ public class JFileManager {
 
 	}
 
+	/*
+	 * Should be do-able without collaboration.
+	 */
 	/**
 	 * Opens the file passed in with a particular application which is also
 	 * passed in.
@@ -201,6 +265,8 @@ public class JFileManager {
 	 * We need to determine what properties we need to add to details. This data
 	 * may be used by other teams, and when we determine what properties we are
 	 * returning, we will need to update it in our documentation.
+	 * 
+	 * Should be do-able without collaboration.
 	 */
 
 	/**
@@ -215,10 +281,10 @@ public class JFileManager {
 	public void getDetails(String name) {
 
 	}
-	
-	
+
 	// We may not use this or put this method's functionality in the getDetails
 	// method.
+	// Should be do-able without collaboration.
 	/**
 	 * Returns the type of the file passed in. This method's functionality will
 	 * change depending on which OS the user is using.
