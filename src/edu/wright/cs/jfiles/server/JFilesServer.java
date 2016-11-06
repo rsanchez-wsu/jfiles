47eded2e0574d0de56b8c5933f202653f64261dd
Samuel Willoughby <willoughby_sam@yahoo.com>
2016-11-06 13:54:17 -0500

    Fixed some minor CheckStyle issues
    The title says it all. There are still a couple remaining that I didn't
    know what to do with.

--- src/edu/wright/cs/jfiles/server/JFilesServer.java
+++ src/edu/wright/cs/jfiles/server/JFilesServer.java
@@ -28,15 +28,12 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import edu.wright.cs.jfiles.common.*;
 
 
