0802206456a265632364a294ffc3de4dcbe14f62
India <India@10.1.58.10>
2017-01-30 19:32:43 -0500

    I created the skeletons for the mv, rm, rmdir, and touch command. I made changes to the errors in the class files and made them checkstyle compliant.

---
+++ src/edu/wright/cs/jfiles/commands/Mv.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2016 - WSU CEG3120 Students
+ *
+ * Roberto C. Sánchez <roberto.sanchez@wright.edu>
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package edu.wright.cs.jfiles.commands;
+/**
+ * The mv command moves or renames a file.
+ * Syntax:
+ * 		MV from to
+ * Example:
+ * 		MV build.xml builds.xml
+ */
+
+public class Mv extends Command {
+
+	/**
+	 * Calls super.
+	 * @param args Command's args.
+	 */
+	public Mv(String args) {
+		super(args);
+	}
+
+	/**
+	 *  TODO: Program for MV.
+	 *  @return Not much yet.
+	 */
+	@Override
+	public String execute() {
+		return "";
+	}
+
+}
---
+++ src/edu/wright/cs/jfiles/commands/Rmdir.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016 - WSU CEG3120 Students
+ *
+ * Roberto C. Sánchez <roberto.sanchez@wright.edu>
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package edu.wright.cs.jfiles.commands;
+
+/**
+ * The rmdir command removes a directory.
+ * Syntax:
+ *      RMDIR directory
+ *  Flags:
+ *      - None.
+ *  Example:
+ *      RMDIR TESTFOLDER
+ */
+
+public class Rmdir extends Command {
+	/**
+	 * Calls super.
+	 * @param args Command's args.
+	 */
+	public Rmdir(String args) {
+		super(args);
+	}
+
+	/**
+	 *  TODO: Program for RMDIR.
+	 *  @return Not much yet
+	 */
+	@Override
+	public String execute() {
+		return "";
+	}
+
+}
