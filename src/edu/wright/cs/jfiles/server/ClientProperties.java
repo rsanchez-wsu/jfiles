ae8e9fbd6504d31eb5f3be589816b7031876d0d5
Logan Rickert <rickert.14@wright.edu>
2017-03-14 00:34:10 -0400

    Added initial ClientProperties

---
+++ src/edu/wright/cs/jfiles/server/ClientProperties.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2017 - WSU CEG3120 Students
+ *
+ *
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package edu.wright.cs.jfiles.server;
+
+import edu.wright.cs.jfiles.database.DatabaseController;
+import edu.wright.cs.jfiles.database.DatabaseUtils.PermissionType;
+import edu.wright.cs.jfiles.database.IdNotFoundException;
+import edu.wright.cs.jfiles.database.User;
+
+/**
+ * Contains client information and properties.
+ */
+public class ClientProperties {
+
+	/**
+	 * < 0 means not logged in.
+	 */
+	private User user = new User();
+	private PermissionType cachePermissionType = PermissionType.NONE;
+
+	/**
+	 * Default constructor for ClientProperties.
+	 */
+	public ClientProperties() {}
+
+	/**
+	 * Logs a user in. Sets userId.
+	 * @param username The username of the user.
+	 * @param password The password of the user.
+	 * @return If login was successful.
+	 */
+	public boolean login(String username, String password) {
+		for (User user : DatabaseController.getUsers()) {
+			if (user.getUsername().equals(username)) {
+				if (user.getPassword().equals(password)) {
+					this.user = user;
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Caches the permissiontype for a certain directory.
+	 * @param dir The directory to cache.
+	 */
+	public void cachePermissionType(String dir) {
+		this.cachePermissionType = getPermissionType(dir);
+	}
+
+	/**
+	 * Returns the current working directory permission type.
+	 * @param type The type to see if they have.
+	 * @return If the user has that permission for current working directory.
+	 */
+	public boolean hasPermission(PermissionType type) {
+		return cachePermissionType == type;
+	}
+
+	/**
+	 * Returns if user has permission.
+	 * @param dir The directory to check for.
+	 * @return If the user has permission.
+	 */
+	public boolean hasPermission(String dir, PermissionType type) {
+		return getPermissionType(dir) == type;
+	}
+
+	/**
+	 * Returns the permission type for the current working directory.
+	 * @return The type of permission.
+	 */
+	public PermissionType getPermissionType() {
+		return cachePermissionType;
+	}
+
+	/**
+	 * Returns the permission type.
+	 * @param dir The directory to be checked.
+	 * @return The type of permission.
+	 */
+	public PermissionType getPermissionType(String dir) {
+		try {
+			return this.user.getId() > 0
+					? DatabaseController.userHasPermission(this.user.getId(), dir)
+					: PermissionType.NONE;
+		} catch (IdNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return PermissionType.NONE;
+		}
+	}
+
+}
